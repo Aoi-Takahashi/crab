43eda49bb5fc729e31c589e187989dc28a55023d
Aoi-Takahashi <pythondevelop.forbusines@gmail.com>
2026-01-03 00:51:22 +0900

    fix: cargo fmt

--- src/storage/file.rs
+++ src/storage/file.rs
@@ -101,7 +101,9 @@ mod tests {
 
     fn home_env_lock() -> std::sync::MutexGuard<'static, ()> {
         static LOCK: OnceLock<Mutex<()>> = OnceLock::new();
-        LOCK.get_or_init(|| Mutex::new(())).lock().expect("lock poisoned")
+        LOCK.get_or_init(|| Mutex::new(()))
+            .lock()
+            .expect("lock poisoned")
     }
 
     struct HomeGuard {
